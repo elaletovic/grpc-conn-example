b8c208cc941fd9596512d8c743534490d8492d20
Emin Laletovic <laletovic@gmail.com>
2023-01-26 21:23:17 +0100

    clean up some mistakes

--- server/go.mod
+++ server/go.mod
@@ -1,3 +1,5 @@
 module github/com/elaletovic/grpc-conn-example/server
 
 go 1.19
+
+require github.com/elaletovic/grpc-conn-example v0.0.0-20230126184136-9ba4ebbf49e6 // indirect
--- server/server.go
+++ server/server.go
@@ -1,9 +1,38 @@
 package server
 
 import (
-	"github.com/elaletovic/grpc-conn-example/communicator"
+	"context"
+	"errors"
+	"fmt"
+	"log"
+
+	comm "github.com/elaletovic/grpc-conn-example/communicator"
+	"google.golang.org/grpc"
 )
 
 type grpcServer struct {
-	communicator.UnimplementedCommunicatorServer
+	comm.UnimplementedCommunicatorServer
+}
+
+func (s *grpcServer) Send(ctx context.Context, req *comm.MessageRequest) (*comm.MessageResponse, error) {
+	response := ""
+	var err error
+	if req != nil {
+		log.Printf("message %s received, payload: %s\n", req.ID, req.Payload)
+		response = fmt.Sprintf("message %s received successfully", req.ID)
+	} else {
+		response = "request cannot be nil"
+		err = errors.New(response)
+	}
+
+	return response, err
+}
+
+func NewServer() *grpcServer {
+	gsrv := grpc.NewServer()
+	srv := grpcServer{}
+
+	comm.RegisterCommunicatorServer(gsrv, &srv)
+
+	return &gsrv
 }
